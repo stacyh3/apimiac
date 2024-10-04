be6715018c1c97fa6a2893813f541f758267a113
Charles Stacy Harris III <stacyh@microsoft.com>
2024-10-04 17:10:40 -0400

    Update name

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "azurerm_resource_group" "rg" {
 
 module "apim" {
   source              = "./modules/apim"
-  name                = "cshtestapim01"
+  name                = "<your name here>"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   publisher_name      = "Star Wars API"
